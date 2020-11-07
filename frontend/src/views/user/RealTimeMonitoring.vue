ce110acb12218e56851854f9a305e8a1b7acfed8
김준호 <tuvmxm@naver.com>
2020-11-08 03:13:17 +0900

    [Feat(F)] RealTime

--- frontend/src/views/user/RealTimeMonitoring.vue
+++ frontend/src/views/user/RealTimeMonitoring.vue
@@ -1,23 +1,28 @@
 <template>
   <div>
     <b>{{ getRealTime }}</b>
+    <AllSchemaStastics />
     <AllSchemaTopQuery />
-    <AllSchemaTopQueryDetail />
+    <AllSchemaTopQueryDetail v-if="getTopQueryDetail" />
   </div>
 </template>
 
 <script>
 import AllSchemaTopQuery from "@/components/realtime-monitoring/query/AllSchemaTopQuery.vue";
 import AllSchemaTopQueryDetail from "@/components/realtime-monitoring/query/AllSchemaTopQueryDetail.vue";
+import AllSchemaStastics from "@/components/realtime-monitoring/schema-status/AllSchemaStastics.vue";
+
 import { mapGetters } from "vuex";
 export default {
   name: "RealTimeMonitoring",
   components: {
     AllSchemaTopQuery,
-    AllSchemaTopQueryDetail
+    AllSchemaTopQueryDetail,
+    AllSchemaStastics
   },
   computed: {
-    ...mapGetters(["getRealTime"])
+    ...mapGetters(["getRealTime"]),
+    ...mapGetters("TopQuery", ["getTopQueryDetail"])
   }
 };
 </script>
