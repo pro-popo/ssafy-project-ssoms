a39295a784d0655fd918297087a8a99607c5a84b
김준호 <tuvmxm@naver.com>
2020-11-09 20:37:48 +0900

    [Style(F)] Top query detail

--- frontend/src/components/realtime-monitoring/query/AllSchemaTopQueryDetail.vue
+++ frontend/src/components/realtime-monitoring/query/AllSchemaTopQueryDetail.vue
@@ -1,30 +1,40 @@
 <template>
-  <div class="realtime-detail-box">
-    <v-card elevation="2" class="realtime-detail-query">
-      <span>
-        executions
-        <div>{{ getTopQueryDetail.executions }}</div>
-      </span>
-      <span
-        >bufferGets
-        <div>{{ getTopQueryDetail.bufferGets }}</div></span
-      >
-      <span
-        >diskReads
-        <div>{{ getTopQueryDetail.diskReads }}</div></span
-      >
-      <span
-        >rowsProcessed
-        <div>{{ getTopQueryDetail.rowsProcessed }}</div></span
-      >
-      <span
-        >cpuTimeAvg
-        <div>{{ getTopQueryDetail.cpuTimeAvg }}</div></span
-      >
-      <span
-        >elapsedTimeAvg
-        <div>{{ getTopQueryDetail.elapsedTimeAvg }}</div></span
-      >
+  <div>
+    <v-card
+      elevation="2"
+      class="realtime-detail-query"
+      height="230px"
+      v-if="getTopQueryDetail"
+    >
+      <div>
+        <div class="query-detail-title">executions</div>
+        <div class="query-detail-text">{{ getTopQueryDetail.executions }}</div>
+        <div class="query-detail-title">bufferGets</div>
+        <div class="query-detail-text">{{ getTopQueryDetail.bufferGets }}</div>
+        <div class="query-detail-title">diskReads</div>
+        <div class="query-detail-text">{{ getTopQueryDetail.diskReads }}</div>
+      </div>
+      <div>
+        <div class="query-detail-title">rowsProcessed</div>
+        <div class="query-detail-text">
+          {{ getTopQueryDetail.rowsProcessed }}
+        </div>
+        <div class="query-detail-title">cpuTimeAvg</div>
+        <div class="query-detail-text">{{ getTopQueryDetail.cpuTimeAvg }}</div>
+        <div class="query-detail-title">elapsedTimeAvg</div>
+        <div class="query-detail-text">
+          {{ getTopQueryDetail.elapsedTimeAvg }}
+        </div>
+      </div>
+    </v-card>
+    <v-card
+      elevation="2"
+      color="#E0E0E0"
+      class="realtime-non-query"
+      height="230px"
+      v-else
+    >
+      <div>자세히 보고싶은 Query를 선택해주세요.</div>
     </v-card>
   </div>
 </template>
@@ -44,13 +54,27 @@ export default {
 .realtime-detail-query {
   display: flex;
   justify-content: space-around;
-  width: 80%;
-  height: 20%;
-  margin: 0 auto;
-  padding: 20px 0;
 }
-.realtime-detail-query > span > div {
+.realtime-non-query {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.realtime-non-query > div {
+  font-weight: 900;
+  opacity: 0.6;
+}
+.query-detail-text {
   font-size: 30px;
   font-weight: 900;
+  color: #6440e3;
+}
+.query-detail-title {
+  opacity: 0.6;
+}
+.realtime-detail-query > div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
