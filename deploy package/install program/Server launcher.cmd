bf870160d71388e85825689877f3753fe0eafce4
Leeyoungbok <dkdlrnf0@gmail.com>
2020-11-10 20:31:22 +0900

    [Fix(F)]|172.30.1.49 to localhost

--- deploy package/install program/Server launcher.cmd
+++ deploy package/install program/Server launcher.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo "������ �����մϴ�."
-start chrome http://172.30.1.49:8080/ssoms
+start chrome http://localhost:8080/ssoms
 java -jar monitoring-0.0.1-SNAPSHOT.war --SetupPath=C:\\Program Files (x86)\\SSOMS
 pause
