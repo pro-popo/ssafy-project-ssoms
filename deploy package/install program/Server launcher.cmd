c1d6c78d0ce6e8364f72c32986f028366b70aaaa
Inho Kim <vrladlsghv@naver.com>
2020-11-26 18:39:18 +0900

    [feat(p)] LAUNCHER

--- deploy package/install program/Server launcher.cmd
+++ deploy package/install program/Server launcher.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo "서버 프로그램을 구동합니다."
 start chrome http://localhost:8080/ssoms
-java -jar monitoring-0.0.1-SNAPSHOT.war --SetupPath=C:\\Program Files (x86)\\SSOMS
+java -jar monitoring-0.0.1-SNAPSHOT.war --SetupPath="C:\\Program Files (x86)\\SSOMS"
 pause
